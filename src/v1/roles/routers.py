acdc5fee1adf4ea11814165a63eb0090532bfc25
sitdaria <sit.ariiina@yandex.ru>
2023-11-21 12:06:21 +0300

    Hren'

--- src/v1/roles/routers.py
+++ src/v1/roles/routers.py
@@ -3,13 +3,20 @@ from typing import Union
 from fastapi import APIRouter, Depends, Path, Query, status
 from typing_extensions import Annotated
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from src.v1.roles.service import PostgreRolesService, get_role_service
+from src.v1.roles.models import Role
+from src.db import postgres
+from src.db.postgres import PostgresDatabase, get_postgres_storage
+
 router = APIRouter(prefix="/roles", tags=["Управление ролями"])
 
-
-
+db_session = postgres.PostgresDatabase()
+DatabaseSession = Annotated[AsyncSession, Depends(db_session)]
 @router.get("/", summary="Получить все роли")
-async def get_roles() -> None:
-    """
-    Mock route.
-    """
+async def get_roles(db: DatabaseSession) -> None:
+    res = await db.execute(select(Role))
+    print(res)
     return None
--- src/v1/roles/service.py
+++ src/v1/roles/service.py
@@ -1,4 +1,14 @@
 from abc import ABC
+from functools import lru_cache
+
+from fastapi import Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing_extensions import Annotated
+
+from src.db import postgres
+from src.db.postgres import PostgresDatabase, get_postgres_storage
+from src.v1.roles.models import Role
 
 
 class BaseRolesService(ABC):
@@ -8,4 +18,18 @@ class BaseRolesService(ABC):
 class PostgreRolesService(BaseRolesService):
     """Role service depends on PostgreSQL"""
 
-    ...
+    session: AsyncSession
+
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    async def get(self):
+        data = await self.session.execute(select(Role))
+        return data
+
+
+@lru_cache()
+def get_role_service(
+    session: AsyncSession,
+) -> PostgreRolesService:
+    return PostgreRolesService(session)
