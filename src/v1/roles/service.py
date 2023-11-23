86a627b482f62c868b2f15dc332a333cc5754456
sitdaria <sit.ariiina@yandex.ru>
2023-11-23 16:31:36 +0300

    Add method get_role and update models Role

--- src/v1/roles/service.py
+++ src/v1/roles/service.py
@@ -18,6 +18,17 @@ from src.v1.roles.schemas import RoleBase, RoleCreate, RoleModify
 class BaseRolesService(ABC):
     """Basic roles service for implement different roles services"""
 
+    @staticmethod
+    async def get_role(session: AsyncSession, obj_id: int) -> Role:
+        """Check if the role exists by id"""
+        statement = select(Role).where(Role.id == obj_id)
+        query = await session.execute(statement)
+        result = query.scalar_one_or_none()
+        if result is None:
+            raise RoleNotFound
+        return result
+
+
     @abstractmethod
     async def get_roles(self, session: AsyncSession) -> List[RoleBase]:
         ...
@@ -52,26 +63,20 @@ class PostgreRolesService(BaseRolesService):
         role = Role(name=RoleCreate(**data).name)
         statement = select(Role).where(Role.name == role_name)
         query = await session.execute(statement)
-        if query.scalar() is not None:
+        if query.scalar():
             raise RoleAlreadyExistsError
 
         try:
             session.add(role)
             await session.commit()
-            statement = select(Role).where(Role.name == role_name)
-            query = await session.execute(statement)
-            new_role = query.scalars().one()
-            return RoleBase.model_validate(new_role)
+            return RoleBase.model_validate(role)
         except exc.SQLAlchemyError:
             await session.rolback()
             raise ServiceError
 
     async def modify_role(self, session: AsyncSession, obj_id: int, data: Mapping) -> RoleBase:
         params = RoleModify(**data)
-        statement = select(Role).filter(Role.id == obj_id)
-        query = await session.execute(statement)
-        if query.scalar() is None:
-            raise RoleNotFound
+        await self.get_role(session, obj_id)
 
         if hasattr(Role, params.name_column):
             statement = (
@@ -86,7 +91,7 @@ class PostgreRolesService(BaseRolesService):
         try:
             query = await session.execute(statement)
             await session.commit()
-            role = query.scalars().one()
+            role = query.scalar_one()
             return RoleBase.model_validate(role)
         except exc.IntegrityError:
             await session.rollback()
@@ -96,19 +101,15 @@ class PostgreRolesService(BaseRolesService):
             raise ServiceError
 
     async def delete_role(self, session: AsyncSession, obj_id: int) -> RoleBase:
-        statement = select(Role).where(Role.id == obj_id)
-        query = await session.execute(statement)
-        if query.scalar() is None:
-            raise RoleNotFound
+        role = await self.get_role(session, obj_id)
 
         try:
             statement = delete(Role).where(Role.id == obj_id).returning(Role)
             query = await session.execute(statement)
             await session.commit()
-            role = query.scalars().one()
+            role = query.scalar_one()
             return RoleBase.model_validate(role)
         except exc.SQLAlchemyError as e:
-            print(e)
             await session.rollback()
             raise ServiceError
 
