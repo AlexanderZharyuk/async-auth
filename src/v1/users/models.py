f325046cfc550bec59c3747ff49b69b6b335e48b
sitdaria <sit.ariiina@yandex.ru>
2023-11-24 15:01:11 +0300

    Update models

--- src/v1/users/models.py
+++ src/v1/users/models.py
@@ -1,12 +1,12 @@
 import uuid
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 from sqlalchemy import UUID, Boolean, DateTime, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models import Base, TimeStampedMixin
-from src.v1.roles.models import RolesToUsers
+from src.v1.roles.models import Role, roles_to_users
 
 
 class User(Base, TimeStampedMixin):
@@ -25,7 +25,7 @@ class User(Base, TimeStampedMixin):
     )
     signature: Mapped[str] = relationship("UsersSignatures", uselist=False, back_populates="user")
 
-    roles: Mapped[List[RolesToUsers]] = relationship()
+    roles: Mapped[List["Role"]] = relationship(secondary=roles_to_users, back_populates="users")
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, login={self.username!r}, name={self.full_name!r}, email={self.email!r})"
