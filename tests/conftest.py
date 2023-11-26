37f0bbe23bb8647245f74756a18b7baa1a9f8b4c
sitdaria <sit.ariiina@yandex.ru>
2023-11-26 16:24:21 +0300

    DB connection added to fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,9 @@ import pytest
 import pytest_asyncio
 
 from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+#ToDo: Брать настройки из файла для тестов (путь подключения к тестовой БД)
+from src.core.config import settings
 
 from src.main import app
 
@@ -18,3 +21,12 @@ async def api_session():
     client = AsyncClient(app=app, base_url="http://api_test")
     yield client
     await client.aclose()
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def db_session():
+    engine = create_async_engine(settings.pg_dsn, future=True)
+    async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    session = async_session()
+    yield session
+    await session.close()
